f6cc1ac0fe68b261efd54917cdc2150a4a28634d
Jonah <jonahsatyaanand@gmail.com>
2022-02-10 11:22:19 +0530

    Sample output added.

--- app.js
+++ app.js
@@ -12,3 +12,4 @@ const generateToken = (id) => {
 }
 
 console.log(generateToken(user._id));
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Impka2tka2tka2tka2tka2siLCJpYXQiOjE2NDQ0NzIzMTksImV4cCI6MTY0NzA2NDMxOX0.ycK5iPn-RGUdhrMNL43Tf6YNWzL6sDHPCJGC32EdPsU
